bb172c88671e9b0010d7fb9d6c32b12a7e71ba00
iaromos <69873911+jarcimos@users.noreply.github.com>
2025-04-04 19:48:46 -0400

    made changes to redirect

--- src/Components/Wlcome/Welcome.jsx
+++ src/Components/Wlcome/Welcome.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 import "./Welcome.css";
 
-const Welcome = () => (
-    <div className='wrapper'>
-      <h1>Welcome!</h1>
-      <h3 className='success'>You have successfully logged in.</h3>
-      <button 
-        onClick={() => {
-          localStorage.removeItem("authenticated");
-          window.location.href = "/";
-        }}
-      >
-        Log out
-      </button>
-    </div>
+const Welcome = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("authenticated");
+  
+    navigate("/#/");
+  };
 
-  );
+    return (
+      <div className='wrapper'>
+        <h1>Welcome!</h1>
+        <h3 className='success'>You have successfully logged in.</h3>
+        <button 
+          onClick={handleLogout}
+        >
+          Log out
+        </button>
+      </div>
+    )
+}
+;
 
 export default Welcome
